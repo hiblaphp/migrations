99f3e102180a15bbbbdaf0b6adbfdad5ede95eb3
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2026-05-27 10:47:24 +0800

    init

---
+++ src/Console/Traits/InitializeDatabase.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hibla\Migrations\Console\Traits;
+
+use Hibla\QueryBuilder\DB;
+
+trait InitializeDatabase
+{
+    private function initializeDatabase(): void
+    {
+        try {
+            DB::connection($this->connection)->table('_test_init');
+        } catch (\Throwable $e) {
+            if (! str_contains($e->getMessage(), 'not found')) {
+                throw $e;
+            }
+        }
+    }
+}
